dc340da5d5c7981fe87bdeaacc9a5a1cd1425711
paraspatle-jpg <paras18224827@gmail.com>
2021-12-03 14:02:54 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { Navbar } from "./components/Navbar";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <h2>Waifu GIFs</h2>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="category">
+      <h2>Select the category</h2>
+      <div class="type">
+        <span>SFW</span>
+        <span>NSFW</span>
+      </div>
+    </div>
+  );
+};
